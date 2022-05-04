900bd24be57ce67e9a5e4a446ca4531a8fa5046b
Thiago Henrique Hupner <thihup@gmail.com>
2022-05-04 09:01:03 -0300

    Initial commit

---
+++ src/main/java/dev/thihup/holy/agent/PatchesVisitor.java
@@ -0,0 +1,207 @@
+package dev.thihup.holy.agent;
+
+
+import java.lang.reflect.Modifier;
+import jdk.internal.org.objectweb.asm.ClassVisitor;
+import jdk.internal.org.objectweb.asm.FieldVisitor;
+import jdk.internal.org.objectweb.asm.Handle;
+import jdk.internal.org.objectweb.asm.MethodVisitor;
+import jdk.internal.org.objectweb.asm.Opcodes;
+import jdk.internal.org.objectweb.asm.Type;
+
+public class PatchesVisitor extends ClassVisitor {
+
+    private final String className;
+    private static final int API_VERSION;
+
+    static {
+        int apiVersion;
+        Class<Opcodes> opcodesClass = Opcodes.class;
+        try {
+            apiVersion = opcodesClass.getDeclaredField("ASM9").getInt(null);
+        } catch (Throwable var1) {
+            try {
+                apiVersion = opcodesClass.getDeclaredField("ASM8").getInt(null);
+            } catch (Throwable var2) {
+                try {
+                    apiVersion = opcodesClass.getDeclaredField("ASM7").getInt(null);
+                } catch (Throwable var3) {
+                    try {
+                        apiVersion = opcodesClass.getDeclaredField("ASM6").getInt(null);
+                    } catch (Throwable var4) {
+                        try {
+                            apiVersion = opcodesClass.getDeclaredField("ASM5").getInt(null);
+                        } catch (Throwable var5) {
+                            try {
+                                apiVersion = opcodesClass.getDeclaredField("ASM4").getInt(null);
+                            } catch (Throwable var6) {
+                                apiVersion = 0;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        API_VERSION = apiVersion;
+    }
+
+    protected PatchesVisitor(String className, ClassVisitor classWriter) {
+        super(API_VERSION, classWriter);
+        this.className = className;
+    }
+
+    @Override
+    public FieldVisitor visitField(int access, String name, String descriptor, String signature,
+        Object value) {
+
+        if (className.equals("sun/font/FontDesignMetrics") && name.equals("metricsCache")) {
+            return cv.visitField(Modifier.FINAL ^ access, name, descriptor, signature, value);
+        }
+
+        if (className.equals("javax/swing/text/html/HTMLEditorKit") && name.equals(
+            "defaultFactory")) {
+            return cv.visitField(Modifier.FINAL ^ access, name, descriptor, signature, value);
+        }
+
+        return cv.visitField(access, name, descriptor, signature, value);
+    }
+
+    @Override
+    public MethodVisitor visitMethod(int access, String name, String descriptor, String signature,
+        String[] exceptions) {
+
+        MethodVisitor methodVisitor = cv.visitMethod(access, name, descriptor, signature,
+            exceptions);
+
+        if (className.equals("jdk/internal/reflect/Reflection") && name.equals("filterFields")) {
+            return new MethodVisitor(API_VERSION) {
+                @Override
+                public void visitCode() {
+                    // Field[] filterFields(Class<?> containingClass, Field[] fields) {return fields; }
+                    methodVisitor.visitCode();
+                    methodVisitor.visitVarInsn(Opcodes.ALOAD, 1);
+                    methodVisitor.visitInsn(Opcodes.ARETURN);
+
+                    methodVisitor.visitMaxs(0, 0);
+                    methodVisitor.visitEnd();
+                }
+            };
+        }
+
+        if (className.equals("com/alee/utils/ProprietaryUtils") && name.equals("setupUIDefaults")) {
+            return new MethodVisitor(API_VERSION) {
+                @Override
+                public void visitCode() {
+                    methodVisitor.visitCode();
+
+                    // sun.swing.SwingUtilities2.putAATextInfo(true, table);
+
+                    methodVisitor.visitInsn(Opcodes.ICONST_1);
+                    methodVisitor.visitVarInsn(Opcodes.ALOAD, 0);
+                    methodVisitor.visitMethodInsn(Opcodes.INVOKESTATIC, "sun/swing/SwingUtilities2",
+                        "putAATextInfo",
+                        "(ZLjava/util/Map;)V", false);
+
+                    methodVisitor.visitInsn(Opcodes.RETURN);
+                    methodVisitor.visitMaxs(0, 0);
+                    methodVisitor.visitEnd();
+                }
+            };
+        }
+        if (className.equals("sun/management/RuntimeImpl") && name.equals("getInputArguments")) {
+            return new MethodVisitor(API_VERSION) {
+                @Override
+                public void visitCode() {
+                    methodVisitor.visitCode();
+
+                    // Util.checkMonitorAccess();
+                    // var 0 = new ArrayList(jvm.getVmArguments());
+                    // 0.removeIf("-javaagent"::startWith);
+                    // return Collections.unmodifiableList(0);
+
+                    methodVisitor.visitMethodInsn(Opcodes.INVOKESTATIC, "sun/management/Util",
+                        "checkMonitorAccess", "()V", false);
+
+                    methodVisitor.visitTypeInsn(Opcodes.NEW, "java/util/ArrayList");
+                    methodVisitor.visitInsn(Opcodes.DUP);
+
+                    methodVisitor.visitVarInsn(Opcodes.ALOAD, 0);
+                    methodVisitor.visitFieldInsn(Opcodes.GETFIELD, "sun/management/RuntimeImpl",
+                        "jvm", "Lsun/management/VMManagement;");
+
+                    methodVisitor.visitMethodInsn(Opcodes.INVOKEINTERFACE,
+                        "sun/management/VMManagement",
+                        "getVmArguments", "()Ljava/util/List;", true);
+
+                    methodVisitor.visitMethodInsn(Opcodes.INVOKESPECIAL, "java/util/ArrayList",
+                        "<init>", "(Ljava/util/Collection;)V", false);
+
+                    methodVisitor.visitVarInsn(Opcodes.ASTORE, 1);
+                    methodVisitor.visitVarInsn(Opcodes.ALOAD, 1);
+                    methodVisitor.visitLdcInsn("-javaagent:patches.jar");
+
+                    methodVisitor.visitLdcInsn("-javaagent");
+                    methodVisitor.visitInvokeDynamicInsn("test",
+                        "(Ljava/lang/String;)Ljava/util/function/Predicate;",
+                        new Handle(Opcodes.H_INVOKESTATIC, "java/lang/invoke/LambdaMetafactory",
+                            "metafactory",
+                            "(Ljava/lang/invoke/MethodHandles$Lookup;Ljava/lang/String;Ljava/lang/invoke/MethodType;Ljava/lang/invoke/MethodType;Ljava/lang/invoke/MethodHandle;Ljava/lang/invoke/MethodType;)Ljava/lang/invoke/CallSite;",
+                            false),
+                        Type.getMethodType(Type.BOOLEAN_TYPE, Type.getType(Object.class)),
+                        new Handle(Opcodes.H_INVOKEVIRTUAL, "java/lang/String", "startsWith",
+                            "(Ljava/lang/String;)Z", false),
+                        Type.getMethodType(Type.BOOLEAN_TYPE, Type.getType(String.class))
+                    );
+
+                    methodVisitor.visitMethodInsn(Opcodes.INVOKEINTERFACE, "java/util/List",
+                        "removeIf",
+                        "(Ljava/util/function/Predicate;)Z", true);
+
+                    methodVisitor.visitInsn(Opcodes.POP);
+                    methodVisitor.visitVarInsn(Opcodes.ALOAD, 1);
+                    methodVisitor.visitMethodInsn(Opcodes.INVOKESTATIC, "java/util/Collections",
+                        "unmodifiableList", "(Ljava/util/List;)Ljava/util/List;", false);
+
+                    methodVisitor.visitInsn(Opcodes.ARETURN);
+                    methodVisitor.visitMaxs(0, 0);
+                    methodVisitor.visitEnd();
+                }
+            };
+        }
+
+        return methodVisitor;
+    }
+
+    @Override
+    public void visitEnd() {
+        if (className.equals("com/alee/utils/system/JavaVersion")) {
+            // JavaVersion(String var) { this(); }
+            MethodVisitor mv = cv.visitMethod(Opcodes.ACC_PUBLIC, "<init>",
+                "(Ljava/lang/String;)V", null, null);
+            mv.visitCode();
+            mv.visitVarInsn(Opcodes.ALOAD, 0);
+            mv.visitMethodInsn(Opcodes.INVOKESPECIAL,
+                "com/alee/utils/system/JavaVersion", "<init>", "()V", false);
+            mv.visitInsn(Opcodes.RETURN);
+            mv.visitMaxs(0, 0);
+            mv.visitEnd();
+
+            // int compareVersion(double var0, int var1, int var2) { return compareTo(var0, var1, var2); }
+            MethodVisitor mv2 = cv.visitMethod(Opcodes.ACC_PUBLIC,
+                "compareVersion", "(DII)I", null, null);
+            mv2.visitCode();
+            mv2.visitVarInsn(Opcodes.ALOAD, 0);
+            mv2.visitVarInsn(Opcodes.DLOAD, 1);
+            mv2.visitVarInsn(Opcodes.ILOAD, 3);
+            mv2.visitVarInsn(Opcodes.ILOAD, 4);
+            mv2.visitMethodInsn(Opcodes.INVOKEVIRTUAL,
+                "com/alee/utils/system/JavaVersion", "compareTo", "(DII)I", false);
+            mv2.visitInsn(Opcodes.IRETURN);
+            mv2.visitMaxs(0, 0);
+            mv2.visitEnd();
+        }
+
+        cv.visitEnd();
+    }
+}
---
+++ src/main/java/dev/thihup/holy/agent/Premain.java
@@ -0,0 +1,150 @@
+package dev.thihup.holy.agent;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.instrument.ClassDefinition;
+import java.lang.instrument.ClassFileTransformer;
+import java.lang.instrument.Instrumentation;
+import java.lang.instrument.UnmodifiableClassException;
+import java.security.ProtectionDomain;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+import jdk.internal.org.objectweb.asm.ClassReader;
+import jdk.internal.org.objectweb.asm.ClassWriter;
+import jdk.internal.org.objectweb.asm.commons.ClassRemapper;
+import jdk.internal.org.objectweb.asm.commons.SimpleRemapper;
+
+public class Premain {
+
+    private static Module getModuleOrThrow(String moduleName) {
+        return ModuleLayer.boot().findModule(moduleName)
+            .orElseThrow(() -> new IllegalStateException("Module " + moduleName + " not found"));
+    }
+
+    public static void premain(String agentArgs, Instrumentation inst) {
+        System.out.println("[Holyrics Patcher] Agent loaded");
+
+        addOpens(inst);
+
+        redefineReflection(inst);
+        inst.addTransformer(new HolyPatcher());
+    }
+
+    private static void redefineReflection(Instrumentation inst) {
+        try {
+            //noinspection Java9ReflectionClassVisibility
+            inst.redefineClasses(
+                new ClassDefinition(Class.forName("jdk.internal.reflect.Reflection"),
+                    HolyPatcher.patchClass("jdk/internal/reflect/Reflection",
+                        Objects.requireNonNull(ClassLoader.getSystemResourceAsStream(
+                            "jdk/internal/reflect/Reflection.class")).readAllBytes())));
+        } catch (UnmodifiableClassException | ClassNotFoundException | IOException e) {
+            System.out.println("[Holyrics Patcher] Failed to patch jdk/internal/reflect/Reflection");
+        }
+    }
+
+    private static void addOpens(Instrumentation instrumentation) {
+        ClassLoader systemClassLoader = ClassLoader.getSystemClassLoader();
+
+        Module unnamedModule = systemClassLoader.getUnnamedModule();
+
+        instrumentation.redefineModule(getModuleOrThrow("java.desktop"), Set.of(),
+            Map.of(), Map.of(
+                "java.awt", Set.of(unnamedModule),
+                "java.awt.font", Set.of(unnamedModule),
+                "java.awt.event", Set.of(unnamedModule),
+                "javax.swing", Set.of(unnamedModule),
+                "javax.swing.table", Set.of(unnamedModule),
+                "javax.swing.text", Set.of(unnamedModule),
+                "javax.swing.text.html", Set.of(unnamedModule),
+                "javax.swing.plaf.basic", Set.of(unnamedModule),
+                "sun.swing", Set.of(unnamedModule),
+                "sun.font", Set.of(unnamedModule)
+            ), Set.of(), Map.of());
+
+        try {
+            instrumentation.redefineModule(getModuleOrThrow("java.desktop"), Set.of(),
+                Map.of(), Map.of("sun.awt.X11", Set.of(unnamedModule)), Set.of(), Map.of());
+        } catch (Exception ignored) {
+        }
+
+        instrumentation.redefineModule(getModuleOrThrow("java.base"), Set.of(),
+            Map.of(), Map.of(
+                "java.util", Set.of(unnamedModule),
+                "java.lang", Set.of(unnamedModule),
+                "java.lang.reflect", Set.of(unnamedModule),
+                "java.text", Set.of(unnamedModule),
+                "jdk.internal.org.objectweb.asm", Set.of(unnamedModule),
+                "jdk.internal.org.objectweb.asm.commons", Set.of(unnamedModule)
+            ), Set.of(), Map.of());
+
+        instrumentation.redefineModule(getModuleOrThrow("javafx.web"), Set.of(),
+            Map.of(), Map.of(
+                "com.sun.webkit.dom", Set.of(unnamedModule)
+            ), Set.of(), Map.of());
+    }
+
+    private static class HolyPatcher implements ClassFileTransformer {
+
+        public static final Map<String, String> RENAMES = Map.of(
+            "jdk/nashorn/api/scripting/NashornScriptEngineFactory",
+            "org/openjdk/nashorn/api/scripting/NashornScriptEngineFactory",
+            "jdk/nashorn/api/scripting/ScriptObjectMirror",
+            "org/openjdk/nashorn/api/scripting/ScriptObjectMirror",
+            "jdk/nashorn/api/scripting/ClassFilter",
+            "org/openjdk/nashorn/api/scripting/ClassFilter"
+        );
+        public static final SimpleRemapper REMAPPER = new SimpleRemapper(RENAMES);
+
+        @Override
+        public byte[] transform(ClassLoader loader, String className, Class<?> classBeingRedefined,
+            ProtectionDomain protectionDomain, byte[] classfileBuffer) {
+
+            switch (className) {
+                case "com/alee/utils/system/JavaVersion":
+                    try (InputStream inputStream = Premain.class.getResourceAsStream(
+                        "/dev/thihup/holy/agent/JavaVersion.class")) {
+                        byte[] bytes = Objects.requireNonNull(inputStream).readAllBytes();
+                        return patchClass("com/alee/utils/system/JavaVersion", bytes);
+                    } catch (Exception e) {
+                        System.out.println("[Holyrics Patcher] Failed to patch " + className);
+                        return null;
+                    }
+                case "com/alee/utils/ProprietaryUtils":
+                case "sun/management/RuntimeImpl":
+                case "jdk/internal/reflect/Reflection":
+                case "sun/font/FontDesignMetrics":
+                case "javax/swing/text/html/HTMLEditorKit":
+                    return patchClass(className, classfileBuffer);
+                case "com/limagiran/util/JavaScriptSecure":
+                case "com/limagiran/util/MyClassFilter":
+                    return remapClass(className, classfileBuffer);
+                default:
+                    return null;
+            }
+        }
+
+        private byte[] remapClass(String className, byte[] classfileBuffer) {
+            System.out.println("[Holyrics Patcher] Patching " + className);
+            ClassReader classReader = new ClassReader(classfileBuffer);
+            ClassWriter classWriter = new ClassWriter(classReader, ClassWriter.COMPUTE_FRAMES);
+            ClassRemapper classRemapper = new ClassRemapper(classWriter, REMAPPER);
+
+            classReader.accept(classRemapper, 0);
+
+            return classWriter.toByteArray();
+        }
+
+        static byte[] patchClass(String className, byte[] classfileBuffer) {
+            System.out.println("[Holyrics Patcher] Patching " + className);
+            ClassReader classReader = new ClassReader(classfileBuffer);
+            ClassWriter classWriter = new ClassWriter(classReader, ClassWriter.COMPUTE_FRAMES);
+
+            classReader.accept(new PatchesVisitor(className, classWriter), 0);
+
+            return classWriter.toByteArray();
+        }
+    }
+
+}
