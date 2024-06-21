a5f9468c6f9dcaaf26af65e2b2dc3bf3c2cb1125
Thiago Henrique Hüpner <thihup@gmail.com>
2024-06-21 10:49:49 -0300

    Refactor code

--- src/main/java/dev/thihup/holy/agent/Patches.java
+++ src/main/java/dev/thihup/holy/agent/Patches.java
@@ -1,12 +1,14 @@
 package dev.thihup.holy.agent;
 
-import java.lang.classfile.*;
-import java.lang.classfile.constantpool.MemberRefEntry;
+import java.lang.classfile.AccessFlags;
+import java.lang.classfile.CodeBuilder;
+import java.lang.classfile.FieldTransform;
+import java.lang.classfile.MethodTransform;
 import java.lang.classfile.instruction.InvokeInstruction;
 import java.lang.constant.*;
-import java.lang.invoke.*;
+import java.lang.invoke.LambdaMetafactory;
 import java.lang.reflect.Modifier;
-import java.util.*;
+import java.util.ArrayList;
 import java.util.function.Predicate;
 
 class Patches {
@@ -16,114 +18,74 @@ class Patches {
             LambdaMetafactory.class.describeConstable().orElseThrow(), "metafactory",
             ConstantDescs.CD_CallSite,
             ConstantDescs.CD_MethodType, ConstantDescs.CD_MethodHandle, ConstantDescs.CD_MethodType);
-    
+
     private static final DirectMethodHandleDesc STRING_STARTS_WITH = MethodHandleDesc.ofMethod(
-        DirectMethodHandleDesc.Kind.VIRTUAL,
-        ConstantDescs.CD_String, "startsWith", MethodTypeDesc.of(ConstantDescs.CD_boolean, ConstantDescs.CD_String));
-    
-    private static final DynamicCallSiteDesc PREDICATE_STRING_STARTS_WITH = 
-        DynamicCallSiteDesc.of(
-                METAFACTORY, 
-                "test",
-                MethodTypeDesc.of(PREDICATE_DESC, ConstantDescs.CD_String))
-            .withArgs(MethodTypeDesc.of(ConstantDescs.CD_boolean, ConstantDescs.CD_Object),
-                STRING_STARTS_WITH,
-                STRING_STARTS_WITH.invocationType());
+            DirectMethodHandleDesc.Kind.VIRTUAL,
+            ConstantDescs.CD_String, "startsWith", MethodTypeDesc.of(ConstantDescs.CD_boolean, ConstantDescs.CD_String));
 
-    static byte[] transform(ClassFile classFile, ClassModel classModel) {
-        return classFile.transform(classModel, (classBuilder, classElement) -> {
-            switch (classElement) {
-                // sun/font/FontDesignMetrics::metricsCache
-                case FieldModel fieldModel when fieldModel.fieldName().equalsString("metricsCache") -> classBuilder.transformField(fieldModel, Patches::finalFlagRemover);
+    private static final DynamicCallSiteDesc PREDICATE_STRING_STARTS_WITH =
+            DynamicCallSiteDesc.of(
+                            METAFACTORY,
+                            "test",
+                            MethodTypeDesc.of(PREDICATE_DESC, ConstantDescs.CD_String))
+                    .withArgs(MethodTypeDesc.of(ConstantDescs.CD_boolean, ConstantDescs.CD_Object),
+                            STRING_STARTS_WITH,
+                            STRING_STARTS_WITH.invocationType());
 
-                // javax/swing/text/html/HTMLEditorKit
-                case FieldModel fieldModel when fieldModel.fieldName().equalsString("defaultFactory") -> classBuilder.transformField(fieldModel, Patches::finalFlagRemover);
-
-                // jdk/internal/reflect/Reflection
-                case MethodModel methodModel when methodModel.methodName().equalsString("filterFields") -> classBuilder.transformMethod(methodModel, Patches::returnArgumentWithoutFiltering);
-
-                // com/alee/utils/ProprietaryUtils
-                case MethodModel methodModel when methodModel.methodName().equalsString("setupUIDefaults") -> classBuilder.transformMethod(methodModel, Patches::callSwingUtilities2);
-
-                // sun/management/RuntimeImpl
-                case MethodModel methodModel when methodModel.methodName().equalsString("getInputArguments") -> classBuilder.transformMethod(methodModel, Patches::hideAgentFromCommandLine);
-
-                // uk/co/caprica/vlcj/player/NativeString
-                case MethodModel methodModel when methodModel.methodName().equalsString("getNativeString") -> classBuilder.transformMethod(methodModel, Patches::removeDeprecatedCallJNA);
-                case MethodModel methodModel when methodModel.methodName().equalsString("copyNativeString") -> classBuilder.transformMethod(methodModel, Patches::removeDeprecatedCallJNA);
-                case MethodModel methodModel when methodModel.methodName().equalsString("format") -> classBuilder.transformMethod(methodModel, Patches::removeDeprecatedCallJNA);
-
-                default -> classBuilder.with(classElement);
-            }
-        });
-    }
 
     // https://github.com/caprica/vlcj/commit/66ef1f303b6849fc5e63087bb4a706b320ef056c
-    private static void removeDeprecatedCallJNA(MethodBuilder methodBuilder, MethodElement methodElement) {
-        if (!(methodElement instanceof CodeModel codeModel)) {
-            methodBuilder.with(methodElement);
-            return;
-        }
-        methodBuilder.transformCode(codeModel, (codeBuilder, codeElement) -> {
-            switch (codeElement) {
-                case InvokeInstruction instruction when instruction.name().equalsString("getString") -> {
-                    codeBuilder.pop();
-                    codeBuilder.invokevirtual(ClassDesc.of("com.sun.jna.Pointer"), "getString", MethodTypeDesc.of(ConstantDescs.CD_String, ConstantDescs.CD_long));
-                }
-                default -> codeBuilder.with(codeElement);
+    static MethodTransform removeDeprecatedCallJNA() {
+        return MethodTransform.transformingCode((codeBuilder, codeElement) -> {
+            if (codeElement instanceof InvokeInstruction instruction && instruction.name().equalsString("getString")) {
+                codeBuilder.pop();
+                codeBuilder.invokevirtual(ClassDesc.of("com.sun.jna.Pointer"),
+                        "getString", MethodTypeDesc.of(ConstantDescs.CD_String, ConstantDescs.CD_long));
+                        return;
             }
+            codeBuilder.with(codeElement);
         });
     }
 
-    private static void returnArgumentWithoutFiltering(MethodBuilder methodBuilder, MethodElement methodElement) {
+    static MethodTransform returnArgumentWithoutFiltering() {
         // Field[] filterFields(Class<?> containingClass, Field[] fields) {
         //   return fields;
         //
-        if (!(methodElement instanceof CodeModel)) {
-            methodBuilder.with(methodElement);
-            return;
-        }
-
-        methodBuilder.withCode((codeBuilder) -> codeBuilder.aload(1).areturn());
+        return MethodTransform.transformingCode((codeBuilder, _) -> codeBuilder.aload(1).areturn());
     }
 
-    private static void hideAgentFromCommandLine(MethodBuilder methodBuilder, MethodElement methodElement) {
+    static MethodTransform hideAgentFromCommandLine() {
         // Util.checkMonitorAccess();
         // var _1 = new ArrayList(jvm.getVmArguments());
         // _1.removeIf("-javaagent"::startWith);
         // _1.removeIf("-agentlib"::startWith);
         // _1.removeIf("-agentpath"::startWith);
         // return Collections.unmodifiableList(_1);
-        if (!(methodElement instanceof CodeModel)) {
-            methodBuilder.with(methodElement);
-            return;
-        }
-
         ClassDesc arrayListClass = ArrayList.class.describeConstable().orElseThrow();
         ClassDesc vmManagementDesc = ClassDesc.ofInternalName("sun/management/VMManagement");
-        methodBuilder.withCode(codeBuilder ->
+        return MethodTransform.transformingCode((codeBuilder, _) ->
                 codeBuilder.invokestatic(ClassDesc.ofInternalName("sun/management/Util"), "checkMonitorAccess", ConstantDescs.MTD_void)
-                        .new_(arrayListClass)
-                        .dup()
-                        .aload(0)
-                        .getfield(ClassDesc.ofInternalName("sun/management/RuntimeImpl"),
-                                "jvm", vmManagementDesc)
-                        .invokeinterface(vmManagementDesc,
-                                "getVmArguments", MethodTypeDesc.of(ConstantDescs.CD_List))
-                        .invokespecial(arrayListClass,
-                                ConstantDescs.INIT_NAME, MethodTypeDesc.of(ConstantDescs.CD_void, ConstantDescs.CD_Collection))
-                        .astore(1)
-                        .aload(1)
-                        .loadConstant("-javaagent")
-                        .block(Patches::removeIf)
-                        .loadConstant("-agentlib")
-                        .block(Patches::removeIf)
-                        .loadConstant("-agentpath")
-                        .block(Patches::removeIf)
-                        .pop()
-                        .aload(1)
-                        .invokestatic(ConstantDescs.CD_Collection, "unmodifiableList", MethodTypeDesc.of(ConstantDescs.CD_List, ConstantDescs.CD_List))
-                        .areturn()
+                .new_(arrayListClass)
+                .dup()
+                .aload(0)
+                .getfield(ClassDesc.ofInternalName("sun/management/RuntimeImpl"),
+                        "jvm", vmManagementDesc)
+                .invokeinterface(vmManagementDesc,
+                        "getVmArguments", MethodTypeDesc.of(ConstantDescs.CD_List))
+                .invokespecial(arrayListClass,
+                        ConstantDescs.INIT_NAME, MethodTypeDesc.of(ConstantDescs.CD_void, ConstantDescs.CD_Collection))
+                .astore(1)
+                .aload(1)
+                .loadConstant("-javaagent")
+                .block(Patches::removeIf)
+                .loadConstant("-agentlib")
+                .block(Patches::removeIf)
+                .loadConstant("-agentpath")
+                .block(Patches::removeIf)
+                .pop()
+                .aload(1)
+                .invokestatic(ConstantDescs.CD_Collection,
+                    "unmodifiableList", MethodTypeDesc.of(ConstantDescs.CD_List, ConstantDescs.CD_List))
+                .areturn()
         );
     }
 
@@ -135,23 +97,23 @@ class Patches {
                         MethodTypeDesc.of(ConstantDescs.CD_boolean, PREDICATE_DESC));
     }
 
-    private static void callSwingUtilities2(MethodBuilder methodBuilder, MethodElement methodElement) {
-        if (!(methodElement instanceof CodeModel cm)) {
-            methodBuilder.with(methodElement);
-            return;
-        }
+    static MethodTransform callSwingUtilities2() {
+        return MethodTransform.transformingCode((codeBuilder, _) ->
+                codeBuilder.iconst_1()
+                        .aload(0)
+                        .invokestatic(ClassDesc.ofInternalName("sun/swing/SwingUtilities2"),
+                                "putAATextInfo",
+                                MethodTypeDesc.of(ConstantDescs.CD_void, ConstantDescs.CD_boolean, ConstantDescs.CD_Map))
+                        .return_());
+    }
 
-        methodBuilder.transformCode(cm, (codeBuilder, e) ->
-                    codeBuilder.iconst_1()
-                            .aload(0)
-                            .invokestatic(ClassDesc.ofInternalName("sun/swing/SwingUtilities2"), "putAATextInfo", MethodTypeDesc.of(ConstantDescs.CD_void, ConstantDescs.CD_boolean, ConstantDescs.CD_Map))
-                            .return_());
-                }
-
-    private static FieldBuilder finalFlagRemover(FieldBuilder fieldBuilder, FieldElement fieldElement) {
-        return fieldBuilder.with(switch (fieldElement) {
-            case AccessFlags flags -> AccessFlags.ofField(flags.flagsMask() ^ Modifier.FINAL);
-            default -> fieldElement;
+    static FieldTransform finalFlagRemover() {
+        return ((builder, element) -> {
+            if (element instanceof AccessFlags flags) {
+                builder.with(AccessFlags.ofField(flags.flagsMask() ^ Modifier.FINAL));
+                return;
+            }
+            builder.with(element);
         });
     }
 
--- src/main/java/dev/thihup/holy/agent/Premain.java
+++ src/main/java/dev/thihup/holy/agent/Premain.java
@@ -1,18 +1,10 @@
 package dev.thihup.holy.agent;
 
-import java.lang.classfile.ClassModel;
-import java.lang.classfile.ClassFile;
-import java.lang.classfile.components.ClassRemapper;
-
 import java.io.IOException;
-import java.io.InputStream;
 import java.lang.System.Logger.Level;
-import java.lang.constant.ClassDesc;
 import java.lang.instrument.ClassDefinition;
-import java.lang.instrument.ClassFileTransformer;
 import java.lang.instrument.Instrumentation;
 import java.lang.instrument.UnmodifiableClassException;
-import java.security.ProtectionDomain;
 import java.util.Map;
 import java.util.Set;
 
@@ -22,18 +14,12 @@ public class Premain {
 
     private static final System.Logger LOGGER = System.getLogger(Premain.class.getName());
 
-    enum BytecodeModificationType {
-        REMAP,
-        PATCH
-    }
-
     private static void openPackagesForModule(String moduleName,
                                               Map<String, Set<Module>> packageToModule,
                                               Instrumentation instrumentation) {
         ModuleLayer.boot().findModule(moduleName).ifPresentOrElse(module -> {
             instrumentation.redefineModule(module, Set.of(), Map.of(), packageToModule, Set.of(),
                     Map.of());
-                    module.isN
         }, () -> LOGGER.log(Level.WARNING,
                 "[Holyrics Patcher] Module " + moduleName + " not found"));
     }
@@ -54,9 +40,9 @@ public class Premain {
             //noinspection Java9ReflectionClassVisibility
             ClassDefinition classDefinition = new ClassDefinition(
                     Class.forName("jdk.internal.reflect.Reflection"),
-                    HolyPatcher.patchClass("jdk/internal/reflect/Reflection",
+                    HolyPatcher.patchClass(
                             classfileBuffer,
-                            BytecodeModificationType.PATCH));
+                            HolyPatcher.matchingMethodName("filterFields", Patches.returnArgumentWithoutFiltering())));
             inst.redefineClasses(classDefinition);
         } catch (UnmodifiableClassException | ClassNotFoundException | IOException e) {
             LOGGER.log(Level.WARNING,
@@ -69,99 +55,43 @@ public class Premain {
 
         Module unnamedModule = systemClassLoader.getUnnamedModule();
 
+        Set<Module> modules = Set.of(unnamedModule);
         openPackagesForModule("java.base", Map.of(
-                "java.util", Set.of(unnamedModule),
-                "java.lang", Set.of(unnamedModule),
-                "java.lang.reflect", Set.of(unnamedModule),
-                "java.text", Set.of(unnamedModule)), instrumentation);
+                "java.util", modules,
+                "java.lang", modules,
+                "java.lang.reflect", modules,
+                "java.text", modules), instrumentation);
 
         openPackagesForModule("java.desktop", Map.of(
-                "java.awt", Set.of(unnamedModule),
-                "java.awt.font", Set.of(unnamedModule),
-                "java.awt.event", Set.of(unnamedModule),
-                "javax.swing", Set.of(unnamedModule),
-                "javax.swing.table", Set.of(unnamedModule),
-                "javax.swing.text", Set.of(unnamedModule),
-                "javax.swing.text.html", Set.of(unnamedModule),
-                "javax.swing.plaf.basic", Set.of(unnamedModule),
-                "sun.swing", Set.of(unnamedModule),
-                "sun.font", Set.of(unnamedModule)), instrumentation);
+                "java.awt", modules,
+                "java.awt.font", modules,
+                "java.awt.event", modules,
+                "javax.swing", modules,
+                "javax.swing.table", modules,
+                "javax.swing.text", modules,
+                "javax.swing.text.html", modules,
+                "javax.swing.plaf.basic", modules,
+                "sun.swing", modules,
+                "sun.font", modules), instrumentation);
 
         try {
-            openPackagesForModule("java.desktop", Map.of("sun.awt.X11", Set.of(unnamedModule)),
+            openPackagesForModule("java.desktop", Map.of("sun.awt.X11", modules),
                     instrumentation);
         } catch (Exception ignored) {
         }
 
         try {
-            openPackagesForModule("java.desktop", Map.of("sun.awt.shell", Set.of(unnamedModule)),
+            openPackagesForModule("java.desktop", Map.of("sun.awt.shell", modules),
                     instrumentation);
         } catch (Exception ignored) {
         }
 
         openPackagesForModule("javafx.web", Map.of(
-                "com.sun.webkit.dom", Set.of(unnamedModule)
+                "com.sun.webkit.dom", modules
         ), instrumentation);
 
-    }
 
-    private static class HolyPatcher implements ClassFileTransformer {
 
-        private static final Map<ClassDesc, ClassDesc> RENAMES = Map.of(
-                ClassDesc.ofInternalName("jdk/nashorn/api/scripting/NashornScriptEngineFactory"),
-                ClassDesc.ofInternalName("org/openjdk/nashorn/api/scripting/NashornScriptEngineFactory"),
-                ClassDesc.ofInternalName("jdk/nashorn/api/scripting/ScriptObjectMirror"),
-                ClassDesc.ofInternalName("org/openjdk/nashorn/api/scripting/ScriptObjectMirror"),
-                ClassDesc.ofInternalName("jdk/nashorn/api/scripting/ClassFilter"),
-                ClassDesc.ofInternalName("org/openjdk/nashorn/api/scripting/ClassFilter"),
-                ClassDesc.ofInternalName("jdk/nashorn/internal/objects/Global"),
-                ClassDesc.ofInternalName("org/openjdk/nashorn/internal/objects/Global")
-        );
-
-        @Override
-        public byte[] transform(ClassLoader loader, String className, Class<?> classBeingRedefined,
-                                ProtectionDomain protectionDomain, byte[] classfileBuffer) {
-
-            return switch (className) {
-                case "com/alee/utils/system/JavaVersion" -> {
-                    try (InputStream inputStream = Premain.class.getResourceAsStream(
-                            "/dev/thihup/holy/agent/JavaVersion.class")) {
-                        yield requireNonNull(inputStream).readAllBytes();
-                    } catch (Exception e) {
-                        System.out.println("[Holyrics Patcher] Failed to patch " + className);
-                        yield null;
-                    }
-                }
-                case "com/alee/utils/ProprietaryUtils",
-                        "sun/management/RuntimeImpl",
-                        "jdk/internal/reflect/Reflection",
-                        "sun/font/FontDesignMetrics",
-                        "javax/swing/text/html/HTMLEditorKit",
-                        "uk/co/caprica/vlcj/player/NativeString",
-                        "uk/co/caprica/vlcj/player/direct/DefaultDirectMediaPlayer" ->
-                        patchClass(className, classfileBuffer, BytecodeModificationType.PATCH);
-                case "com/limagiran/js/JavaScriptSecure",
-                        "com/limagiran/js/MyClassFilter",
-                        "com/limagiran/js/JSUtils",
-                        "com/limagiran/holyrics/js/JSLibHolyrics" ->
-                        patchClass(className, classfileBuffer, BytecodeModificationType.REMAP);
-                default -> null;
-            };
-        }
-
-        private static byte[] patchClass(String className, byte[] classfileBuffer,
-                                         BytecodeModificationType changeType) {
-            LOGGER.log(Level.DEBUG, "[Holyrics Patcher] Patching " + className);
-
-            ClassFile classfile = ClassFile.of();
-            ClassModel classModel = classfile.parse(classfileBuffer);
-
-            return switch (changeType) {
-                case REMAP -> ClassRemapper.of(RENAMES).remapClass(classfile, classModel);
-                case PATCH -> Patches.transform(classfile, classModel);
-            };
-
-        }
     }
 
 }
